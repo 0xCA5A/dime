a48e67429573efcd96b1cd2deab277076ecd290f
Samuel Casa <samuel.casa@neratec.com>
2016-04-19 11:44:22 +0200

    Modify start scripts
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- camera_runner.sh
+++ camera_runner.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
+export APP="app.camera_dime"
 
-SCRIPT_NAME="$0"
-SCRIPTS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LIB_DIR="${SCRIPTS_ROOT}/lib/"
-
-export PYTHONPATH=${LIB_DIR}:${PYTHON_PATH}
-PYTHON="$(which python3)"
-
-echo -e "[${SCRIPT_NAME}] start dime"
-${PYTHON} -m app.camera_dime $1 $2 $3 $4
-
-echo -e "[${SCRIPT_NAME}] exit"
+/bin/bash runner.sh $@
--- xmpp_runner.sh
+++ xmpp_runner.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
+export APP="app.xmpp_dime"
 
-SCRIPT_NAME="$0"
-SCRIPTS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LIB_DIR="${SCRIPTS_ROOT}/lib/"
-
-export PYTHONPATH=${LIB_DIR}:${PYTHON_PATH}
-PYTHON="$(which python3)"
-
-echo -e "[${SCRIPT_NAME}] start dime"
-${PYTHON} -m app.xmpp_dime $1 $2 $3 $4
-
-echo -e "[${SCRIPT_NAME}] exit"
+/bin/bash runner.sh $@
