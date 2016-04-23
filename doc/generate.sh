c3531e7823941ede6f73335102bb010229485115
Samuel Casa <samuelcasa9@gmail.com>
2016-04-23 10:11:10 +0200

    fixup! Add pyreverse doc generator to keep the overview

--- doc/generate.sh
+++ doc/generate.sh
@@ -2,14 +2,14 @@
 
 ALL_PY_FILES=$(find ../ -name "*.py")
 
-# General Vue on a Module
+echo -e "General Vue on a Module"
 pyreverse -ASmy -k -o pdf ${ALL_PY_FILES} -p general_vue
 
-# Detailed Vue on a Module
+echo -e "Detailed Vue on a Module"
 # pyreverse -c module_one -a1 -s1 -f ALL -o pdf ${ALL_PY_FILES}
 
-#default
+echo -e "Default"
 pyreverse -o pdf -p default ${ALL_PY_FILES}
 
-#default but with all attributes
+echo -e "Default but with all attributes"
 pyreverse -o pdf -p default_all_attributes -f ALL ${ALL_PY_FILES}
