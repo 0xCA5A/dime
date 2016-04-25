86226ec327495b8e88264f3a92d9654c967dbaac
Samuel Casa <samuelcasa9@gmail.com>
2016-04-25 18:10:09 +0200

    Add virtualenv configuration to start scripts
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- runner.sh
+++ runner.sh
@@ -4,6 +4,17 @@ SCRIPT_NAME="$0"
 SCRIPTS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LIB_DIR="${SCRIPTS_ROOT}/lib/"
 
+# virtualenv setup
+if [ ! -z ${VIRTUALENV_CFG+x} ]; then
+    echo "[${SCRIPT_NAME}] configure virtualenv using file ${VIRTUALENV_CFG}"
+    virtualenv /tmp/venv
+    source /tmp/venv/bin/activate
+    pip3 install -r ${VIRTUALENV_CFG}
+
+    [ $? -eq 0 ] || (echo -e "[${SCRIPT_NAME}] problems with virtualenv setup, exit immediately"; exit 1)
+fi
+[ $? == 1 ] && exit 0;
+
 export PYTHONPATH=${LIB_DIR}:${PYTHON_PATH}
 PYTHON="$(which python3)"
 
--- schedule_runner.sh
+++ schedule_runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export APP="app.schedule_dime"
+export VIRTUALENV_CFG="venv_schedule_dime.req"
 
 /bin/bash runner.sh $@
